a5c3993ebcb4b87902a3784d794121c9d1a5ea0d
Jayjay <timothyjapolinario@gmail.com>
2022-02-04 22:51:57 +0800

    Fixed iTuneMovie responses

--- app/src/main/java/com/example/itunesmovies/data/responses/iTuneMovie.kt
+++ app/src/main/java/com/example/itunesmovies/data/responses/iTuneMovie.kt
@@ -1,31 +1,6 @@
 package com.example.itunesmovies.data.responses
 
 data class iTuneMovie(
-    val artistName: String,
-    val artworkUrl100: String,
-    val artworkUrl30: String,
-    val artworkUrl60: String,
-    val collectionExplicitness: String,
-    val collectionHdPrice: Double,
-    val collectionPrice: Double,
-    val contentAdvisoryRating: String,
-    val country: String,
-    val currency: String,
-    val kind: String,
-    val longDescription: String,
-    val previewUrl: String,
-    val primaryGenreName: String,
-    val releaseDate: String,
-    val shortDescription: String,
-    val trackCensoredName: String,
-    val trackExplicitness: String,
-    val trackHdPrice: Double,
-    val trackHdRentalPrice: Double,
-    val trackId: Int,
-    val trackName: String,
-    val trackPrice: Double,
-    val trackRentalPrice: Double,
-    val trackTimeMillis: Int,
-    val trackViewUrl: String,
-    val wrapperType: String
+    val resultCount: Int,
+    val results: List<Result>
 )
---
+++ app/src/main/java/com/example/itunesmovies/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.itunesmovies.util
+
+object Constants {
+    const val BASE_URL = "https://itunes.apple.com/search?"
+
+}
